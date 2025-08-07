379f4f8ffb877f689d6f516e397fc3f1603b079c
Acer <Acer@DESKTOP-RNJI9QU>
2025-08-08 00:03:58 +0530

    Updated

---
+++ src/com/tnsif/Day2/DecisionMakingWithOperators.java
@@ -0,0 +1,24 @@
+package com.tnsif.Day2;
+
+public class DecisionMakingWithOperators {
+
+	public static void main(String[] args) {
+		int x = 6 , y = 5;
+		int a = 12;
+		int b =13;
+	    if(x>=y) {
+	    	System.out.println("TRUE");
+	    }
+	    else {
+	    	System.out.println("FALSE");
+	    }
+	    if((a<b) || (a==b)) {
+	    	System.out.println("TRUE");
+	    }
+	    else {
+	    	System.out.println("FALSE");
+	    }
+	    
+	}
+
+}
---
+++ src/com/tnsif/Day2/DoWhile.java
@@ -0,0 +1,15 @@
+package com.tnsif.Day2;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int a= 4;
+		do {
+			System.out.println(a);
+			a++;
+		}while(a<=1);
+		
+
+	}
+
+}
